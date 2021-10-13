36b4dbb14bdc4a33e25912fdeed2f92b65a59779
masonbrad831 <masonbrad831@gmail.com>
2021-10-13 16:01:40 -0600

    UI & TEXT-TO-SPEECH
    I added a pretty good starting point for the UI with slight animations. The text to speech is working great but can use some optimization to speed it up slightly.

--- Capstone/MainWindow.xaml.cs
+++ Capstone/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Watson.TextToSpeech.v1;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +27,78 @@ namespace Capstone
         {
             InitializeComponent();
         }
+
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_contacts.Visibility = Visibility.Collapsed;
+                tt_messages.Visibility = Visibility.Collapsed;
+                tt_maps.Visibility = Visibility.Collapsed;
+                tt_settings.Visibility = Visibility.Collapsed;
+                tt_signout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_contacts.Visibility = Visibility.Visible;
+                tt_messages.Visibility = Visibility.Visible;
+                tt_maps.Visibility = Visibility.Visible;
+                tt_settings.Visibility = Visibility.Visible;
+                tt_signout.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+
+        //}
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    API api = new API();
+        //    //api.SaveSound(input, dropdown);
+        //    api.PlaySound();
+        //    api.DeleteSound("outputWAV.wav", @"C:\temp");
+        //    api.DeleteSound("output.wav", @"C:\temp");
+        //    //webBrowser1.Source = new Uri("https://www.google.com/maps/place/" + input.Text);
+        //}
+
+        //private void dropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+
+        //}
     }
 }
+
